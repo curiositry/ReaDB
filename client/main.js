941d22addd0ed280084ffd5601140eac0c7e6511
Omphalosskeptic <isa@automathic.org>
2015-09-11 14:16:16 -0700

    ISBN lookup with Google Books API! Woot! (plus a bunch of other improvements…)

--- client/main.js
+++ client/main.js
@@ -65,6 +65,10 @@ function fetchBooks () {
   return Books.find({"meta.userId": Meteor.userId()}).fetch()
 }
 
+function deleteBook (bookId) {
+  Books.remove({_id:bookId});
+}
+
 function toJSONString(data){
   var str = JSON.stringify(data);
   console.log(str);
@@ -105,6 +109,7 @@ Template.bookList.helpers({
 Template.header.helpers({
   notification: function(){
     return Session.get("notification");
+    Session.set("notification", "");
   }
 })
 
@@ -112,6 +117,15 @@ Template.viewBook.helpers({
   book: function(){
     var bookId = Session.get("bookId");
     return Books.findOne({_id: bookId});
+  }
+});
+
+Template.viewBook.events({
+  "click #deleteBook": function(){
+    var bookId = Session.get("bookId");
+    deleteBook(bookId);
+    Session.set("notification", "☑ Sucessfully deleted book: " +bookId);
+    Router.go("/");
   }
 });
 
@@ -140,21 +154,81 @@ Template.exportCSV.events({
 });
 
 Template.addBook.events({
+  "click #fetchFromISBN": function(event, template){
+    event.preventDefault();
+    var isbn = document.getElementById("isbnInput").value;
+    var title = document.getElementById("titleInput").value;
+    var author = document.getElementById("authorInput").value;
+    
+    console.log(isbn);
+    console.log(title);
+    console.log(author);
+    
+    
+    Meteor.call("fetchBookMetadata", isbn, title, author, function(error, result){
+      if(result){
+        var metadata = JSON.parse(result.content).items[0];
+        
+        var isbnInput = document.getElementById('isbnInput');
+        var titleInput = document.getElementById('titleInput');
+        var authorInput = document.getElementById('authorInput');
+        var publicationDateInput = document.getElementById('publicationDateInput');
+        var publisherDescriptionInput = document.getElementById('publisherDescriptionInput');
+        var tagsInput = document.getElementById('tagsInput');
+        var ratingInput = document.getElementById('ratingInput');
+        var imgUrlInput = document.getElementById('imgUrlInput');
+        var pageCountInput = document.getElementById('pageCountInput')
+        
+        if(!isbn){
+          if (metadata.volumeInfo.industryIdentifiers[1]){
+            isbnInput.value = metadata.volumeInfo.industryIdentifiers[1].identifier;
+          }else {
+            isbnInput.value = metadata.volumeInfo.industryIdentifiers[0].identifier;
+          }
+        }      
+        titleInput.value = metadata.volumeInfo.title;
+        authorInput.value = metadata.volumeInfo.authors;
+        publicationDateInput.value = metadata.volumeInfo.publishedDate;      
+        publisherDescriptionInput.value = metadata.volumeInfo.description;
+        tagsInput.value = metadata.volumeInfo.categories;
+        ratingInput.value =  metadata.volumeInfo.averageRating;
+        imgUrlInput.value = metadata.volumeInfo.imageLinks.thumbnail;
+        pageCountInput.value = metadata.volumeInfo.pageCount;
+        
+      }
+      if(error){
+        Session.set("notification", "Error fetching metadata from ISBN: "+error);
+      }
+    });
+    
+  },
   "submit form": function(event, template){
     event.preventDefault();
+    d = new Date();
+    var dateAdded = d.yyyymmdd();
     Books.insert({
       "bookid": null,
       "isbn": event.target.isbn.value, 
       "title": event.target.title.value,
       "author": event.target.author.value,
-      "img": "",
+      "dateRead": event.target.dateRead.value,
+      
       "review": event.target.review.value, 
       "notes": event.target.notes.value, 
       "rating": event.target.rating.value,
       "tags": event.target.tags.value, 
+      "format": event.target.format.value,
       "meta": {
-        "userId": Meteor.userId()
+        "userId": Meteor.userId(),
+        "dateAdded": dateAdded,
+        "imgUrl": event.target.imgUrl.value,
+        "pubdate": event.target.pubdate.value,
+        "publisherDescription": event.target.publisherDescription.value,
+        "pageCount": event.target.pageCount.value,
+        "imgUrl": event.target.imgUrl.value
       }
-    })
+    });
+    Session.set("notification", "☑ "+event.target.title.value+" sucessfully added to database");
+    Router.go("/");
   }
 })
