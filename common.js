01a32615787614760e7879c8ae974af9bb352f45
Omphalosskeptic <isa@automathic.org>
2015-09-11 14:16:39 -0700

    Moved pretty date function to common.js so I could use it on the client, too.

--- common.js
+++ common.js
@@ -1,1 +1,8 @@
 Books = new Mongo.Collection('books');
+
+Date.prototype.yyyymmdd = function() {
+  var yyyy = this.getFullYear().toString();
+  var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+  var dd  = this.getDate().toString();
+  return yyyy +"-"+ (mm[1]?mm:"0"+mm[0]) +"-"+ (dd[1]?dd:"0"+dd[0]); // padding
+};
