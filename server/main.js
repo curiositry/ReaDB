9fc07104aa926b8a1a151bc74e6cd37a59aae9bd
Omphalosskeptic <isa@automathic.org>
2015-09-22 12:07:57 -0700

    Added code for updating metadata for whole library (not finished)

--- server/main.js
+++ server/main.js
@@ -1,14 +1,11 @@
 // Books.remove({})
 
-
-
 function fetchFromAPI(url) {
   console.log("fetchFromAPI function called");
   console.log(url);
   // synchronous GET
   try {
     var result = Meteor.http.get(url, {timeout:30000});
-    console.log(result);
     return result;
   } catch (e) {
     return e;
@@ -24,6 +21,20 @@ function fetchFromAPI(url) {
   // }
 }
 
+function fetchBookMetadata(isbn, title, author){
+  console.log("fetchBookMetadata function called");
+  var url;
+  if(isbn){
+    url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn;
+  }if (title && author) {
+    url = "https://www.googleapis.com/books/v1/volumes?q=title:"+title+"+inauthor:"+author;
+  }else {
+    url = "https://www.googleapis.com/books/v1/volumes?q=title:"+title;
+  }
+  url = "http://localhost:3000/response.json";
+  var result = fetchFromAPI(url);
+  return result;
+}
 
 importCSV = function(file) {
   console.log("enter function import_file_orders")
@@ -92,16 +103,67 @@ Meteor.methods({
       console.log("completed");
     },
     fetchBookMetadata: function(isbn, title, author){
-      console.log("fetchBookMetadata method called");
-      if(isbn){
-        var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn;
-      }if (title && author) {
-        var url = "https://www.googleapis.com/books/v1/volumes?q=title:"+title+"+inauthor:"+author;
-      }else {
-        var url = "https://www.googleapis.com/books/v1/volumes?q=title:"+title;
-      }
+      console.log("e");
       this.unblock();
-      var result = fetchFromAPI(url);
-      return result;
+      return fetchBookMetadata(isbn, title, author);
+    },
+    updateLibraryMetadata: function(){
+      // this.unblock();
+      console.log("update lib meta called");
+      var library = Books.find({}).fetch(); 
+      for(var i = 0; i < library.length; i++){
+        console.log("in for loop");
+        var book = library[i];
+        console.log(book.title);
+        console.log(book.meta);
+        if (!book.meta.pubdate){   
+          this.unblock();    
+          var result = fetchBookMetadata(book.isbn, book.title, book.author);
+          var metadata = JSON.parse(result.content).items[0];
+          var isbn = book.isbn;   
+          console.log(book.title);
+
+          if(!book.isbn){
+            if (metadata.volumeInfo.industryIdentifiers[1]){
+              isbn = metadata.volumeInfo.industryIdentifiers[1].identifier;
+            }else {
+              isbn = metadata.volumeInfo.industryIdentifiers[0].identifier;
+            }
+          }
+             
+          Books.update({_id: book._id},{$set: {             
+            "isbn": isbn, 
+            "title": metadata.volumeInfo.title,
+            "author": metadata.volumeInfo.authors,        
+            "meta": {
+              "userId": Meteor.userId(),
+              "dateAdded": book.meta.dateAdded,
+              "dateModified": dateModified,
+              "imgUrl":  metadata.volumeInfo.imageLinks.thumbnail,
+              "pubdate": metadata.volumeInfo.publishedDate,
+              "publisherDescription": metadata.volumeInfo.description,
+              "pageCount": metadata.volumeInfo.pageCount
+            }
+          }}); 
+          
+          d = new Date();
+          var dateModified = d.yyyymmdd();
+                 
+          // Books.update({_id: "ioq2YHB3gdy2vzxue"},{$set: {             
+          //   "isbn": "123123123", 
+          //   "title": "Title",
+          //   "author": "metadata.volumeInfo.authors",        
+          //   "meta": {
+          //     "userId": Meteor.userId(),
+          //     "dateAdded": book.meta.dateAdded,
+          //     "dateModified": dateModified,
+          //     "imgUrl":  "metadata.volumeInfo.imageLinks.thumbnail",
+          //     "pubdate": "metadata.volumeInfo.publishedDate",
+          //     "publisherDescription": "metadata.volumeInfo.description",
+          //     "pageCount": "metadata.volumeInfo.pageCount"
+          //   }
+          // }});               
+        }
+      }
     }
 });
