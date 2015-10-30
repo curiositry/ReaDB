974831c68dbc6a2001678ba21163a51a55a266c0
Curiositry <isa@automathic.org>
2015-10-30 10:23:59 -0600

    Added Bert defaults

---
+++ client/main.js
@@ -0,0 +1,9 @@
+Bert.defaults = {
+  hideDelay: 5000,
+  // Accepts: a number in milliseconds.
+  style: 'growl-top-right',
+  // Accepts: fixed-top, fixed-bottom, growl-top-left,   growl-top-right,
+  // growl-bottom-left, growl-bottom-right.
+  type: 'default'
+  // Accepts: default, success, info, warning, danger.
+};
