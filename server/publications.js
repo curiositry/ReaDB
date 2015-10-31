086eca8b280cd70c7a14f3185abba8851656b299
Curiositry <isa@automathic.org>
2015-10-30 22:04:03 -0600

    Publish temp data that belongs to current user to app client

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('userBooks', function() {  
   return Books.find({"meta.userId":this.userId});
 });
+
+Meteor.publish('userTemp', function() {  
+  return Temp.find({"userId":this.userId});
+});
