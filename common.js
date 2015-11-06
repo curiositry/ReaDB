64fabb682a68f878fdb17ce12028e667ebac7ced
Curiositry <isa@automathic.org>
2015-11-06 13:14:20 -0700

    Fix easy:searcg implementation (+ using selector to only return docs that blong to current user)

--- common.js
+++ common.js
@@ -3,7 +3,33 @@ Books = new Mongo.Collection('books');
 BooksIndex = new EasySearch.Index({
   collection: Books,
   fields: ['title'],
-  engine: new EasySearch.MongoDB()
+  engine: new EasySearch.MongoDB({
+    sort: () => ['rating'],
+    selector: function (searchObject, options, aggregation) {
+      let selector = this.defaultConfiguration().selector(searchObject, options, aggregation),
+        categoryFilter = options.search.props.categoryFilter;
+
+      if (_.isString(categoryFilter) && !_.isEmpty(categoryFilter)) {
+        selector.category = categoryFilter;
+      }
+      selector["meta.userId"] = options.search.userId;
+      
+      // selector["$or"] = [{"title":"rework"}];
+      // 
+      console.log(options.search.userId);
+      console.log(JSON.stringify(selector));
+
+      return selector;
+    }
+  }),
+  'query': function(searchString, options) {
+    var query = EasySearch.getSearcher(this.use).defaultQuery(this, searchString);
+    var userId = options.searcg.userId;
+    console.log(userId);
+    query["title"] = "rework";
+    console.log(query);
+    return query;
+  }
 });
 
 Temp = new Mongo.Collection('temp');
