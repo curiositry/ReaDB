ada962e81853b3707a9db83aa6a59cc352b97186
Curiositry <isa@automathic.org>
2015-12-02 15:43:51 -0800

    Update truncate and slugify helpers

--- client/helpers.js
+++ client/helpers.js
@@ -1,14 +1,10 @@
-Handlebars.registerHelper('truncate', function(passedString, endstring, endchar) {
-  // Trim string
-  var theString = passedString.substring( 0, endstring, endchar );
-  // Don’t trim in the middle of a word…
-  theString = theString.substr(0, Math.min(theString.length,         theString.lastIndexOf(" ")));
-   if ( passedString.length > theString.length && endchar) {
-     theString += endchar;
-   }
-   return new Handlebars.SafeString(theString);
+Handlebars.registerHelper('truncate', function(str, end, char) {
+   return new Handlebars.SafeString(truncate(str, end, char));
 });
 
+Handlebars.registerHelper('slugify', function(str) {
+   return new Handlebars.SafeString(slugify(str));
+});
 
 Handlebars.registerHelper('replaceLinebreaks', function(passedString, replacement) {
   var theString = passedString.replace(/\n/g,"<br>");
