adf3f88b5ca98fb376a8229f01e17faf79adc284
Curiositry <isa@automathic.org>
2015-12-08 09:38:41 -0800

    Added "plural" helper

--- client/helpers.js
+++ client/helpers.js
@@ -6,6 +6,17 @@ Handlebars.registerHelper('slugify', function(str) {
    return new Handlebars.SafeString(slugify(str));
 });
 
+Handlebars.registerHelper('plural', function(num, nill, singular, plural) {
+   if (num == 0 || num == undefined) {
+     return  new Handlebars.SafeString(nill);
+   } else if (num == 1) {
+     return  new Handlebars.SafeString(singular);
+   } else if (num > 1) {
+     return  new Handlebars.SafeString(plural);
+   }
+});
+
+
 Handlebars.registerHelper('replaceLinebreaks', function(passedString, replacement) {
   var theString = passedString.replace(/\n/g,"<br>");
    return new Handlebars.SafeString(theString)
